65a07b520f535c7256402718ac8b0c776d1e703c
Charles Bell <cb8@users.noreply.github.com>
2017-02-04 00:38:38 +0100

    Update test classes to PSR-4

--- test/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$autoload_path =  __DIR__ . "/../vendor/autoload.php";
-$loader = require($autoload_path);
-
-$loader->add('CentralDesktop\JMX\Test', __DIR__);
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-namespace CentralDesktop\JMX\Test;
+namespace CentralDesktop\JMX\Tests;
 
 
 
